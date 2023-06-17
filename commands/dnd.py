68a3f05b23e3ae555e87510e4a50bde6779599c1
Trevor Oler <18trevor3695@gmail.com>
2023-06-17 01:52:13 -0400

    Update dnd.py

--- commands/dnd.py
+++ commands/dnd.py
@@ -1,9 +1,20 @@
 from discord.ext import commands
 import random
 
+class Character:
+    def __init__(self, name, race, character_class, level):
+        self.name = name
+        self.race = race
+        self.character_class = character_class
+        self.level = level
+
+    def __str__(self):
+        return f"Name: {self.name}\nRace: {self.race}\nClass: {self.character_class}\nLevel: {self.level}"
+
 class DND(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.characters = {}  # Store characters as {user_id: Character}
 
     @commands.command()
     async def attack(self, ctx):
@@ -17,6 +28,28 @@ class DND(commands.Cog):
         spells = ['Fireball', 'Magic Missile', 'Healing Touch']
         spell_cast = random.choice(spells)
         await ctx.send(f"You cast {spell_cast}!")
+
+    @commands.command()
+    async def create_character(self, ctx, name, race, character_class, level=1):
+        """Creates a D&D character for the user."""
+        user_id = ctx.author.id
+        if user_id in self.characters:
+            await ctx.send("You already have a character!")
+            return
+
+        character = Character(name, race, character_class, level)
+        self.characters[user_id] = character
+        await ctx.send(f"Character '{name}' created for user {ctx.author.mention}!")
+
+    @commands.command()
+    async def show_character(self, ctx):
+        """Displays the character of the user."""
+        user_id = ctx.author.id
+        character = self.characters.get(user_id)
+        if character:
+            await ctx.send(f"User {ctx.author.mention}'s character:\n{character}")
+        else:
+            await ctx.send("You haven't created a character yet!")
 
 async def setup(bot):
     await bot.add_cog(DND(bot))
