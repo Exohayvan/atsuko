1f937e256f71bca152f7c1106ec9bc23c95e4f88
Trevor Oler <18trevor3695@gmail.com>
2023-06-16 22:58:21 -0400

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -4,32 +4,20 @@ import datetime
 class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.start_time = self.load_start_time()
-
-    def load_start_time(self):
-        try:
-            with open("start_time.txt", "r") as f:
-                start_time_str = f.read()
-                return datetime.datetime.fromisoformat(start_time_str)
-        except FileNotFoundError:
-            return datetime.datetime.utcnow()
-
-    def save_start_time(self):
-        with open("start_time.txt", "w") as f:
-            start_time_str = self.start_time.isoformat()
-            f.write(start_time_str)
+        self.start_time = datetime.datetime.utcnow()
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.start_time = self.load_start_time()
+        self.start_time = datetime.datetime.utcnow()
 
     @commands.command()
     async def uptime(self, ctx):
-        """Shows the current uptime of the bot."""
+        """Shows the current uptime of the bot since last reboot."""
         delta = datetime.datetime.utcnow() - self.start_time
-        hours, remainder = divmod(int(delta.total_seconds()), 3600)
-        minutes, seconds = divmod(remainder, 60)
-        days, hours = divmod(hours, 24)
+        days, seconds = delta.days, delta.seconds
+        hours = days * 24 + seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = seconds % 60
 
         await ctx.send(f"Current Uptime: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds.")
 
