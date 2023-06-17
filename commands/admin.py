0ee2918a17feba9b1c4fc6e2b5f33430129495ab
Trevor Oler <18trevor3695@gmail.com>
2023-06-16 23:59:56 -0400

    Update admin.py

--- commands/admin.py
+++ commands/admin.py
@@ -27,35 +27,17 @@ class AdminCommands(commands.Cog):
             await ctx.send("You don't have permission to use this command.")
             return
 
-        # Initialize a GitHub API object using your token
-        g = Github(GITHUB_TOKEN)
-
-        # Get the repository
-        repo = g.get_repo(GITHUB_REPO)
-
-        # Get the name of the latest commit
-        latest_commit = repo.get_commits()[0].sha
-
-        # Check if the latest commit is the currently running code
-        if os.environ.get('CURRENT_COMMIT') != latest_commit:
-            # If not, update the repository URL with the GitHub token
-            repo_url = GITHUB_REPO
-            new_repo_url = repo_url.replace("https://", f"https://{GITHUB_TOKEN}@github.com/")
-            subprocess.run(["git", "remote", "set-url", "origin", new_repo_url], check=True)
-
-            # Then, pull the latest code and update the environment variable
-            result = subprocess.run(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output = result.stdout.decode("utf-8")
-            error = result.stderr.decode("utf-8")
-            os.environ['CURRENT_COMMIT'] = latest_commit
-            if error:
-                await ctx.send(f'Update failed with error: {error}')
-            else:
-                await ctx.send(f'Update successful: {output}')
-                await ctx.send('Bot restarting.')
-                sys.exit(RESTART_EXIT_CODE)
+        # Then, pull the latest code
+        result = subprocess.run(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode("utf-8")
+        error = result.stderr.decode("utf-8")
+    
+        if error:
+            await ctx.send(f'Update failed with error: {error}')
         else:
-            await ctx.send('Already up to date.')
+            await ctx.send(f'Update successful: {output}')
+            await ctx.send('Bot restarting.')
+            sys.exit(RESTART_EXIT_CODE)
 
     @commands.command()
     async def restart(self, ctx):
