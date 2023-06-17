fd3364f290d602b20d476919d56929d32e637d6c
Trevor Oler <18trevor3695@gmail.com>
2023-06-16 22:48:34 -0400

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -4,7 +4,24 @@ import datetime
 class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.start_time = datetime.datetime.utcnow()
+        self.start_time = self.load_start_time()
+
+    def load_start_time(self):
+        try:
+            with open("start_time.txt", "r") as f:
+                start_time_str = f.read()
+                return datetime.datetime.fromisoformat(start_time_str)
+        except FileNotFoundError:
+            return datetime.datetime.utcnow()
+
+    def save_start_time(self):
+        with open("start_time.txt", "w") as f:
+            start_time_str = self.start_time.isoformat()
+            f.write(start_time_str)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.start_time = self.load_start_time()
 
     @commands.command()
     async def uptime(self, ctx):
@@ -27,6 +44,9 @@ class Info(commands.Cog):
         seconds = seconds % 60
 
         await ctx.send(f"Total Lifetime Uptime: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds.")
+
+    def cog_unload(self):
+        self.save_start_time()
 
 async def setup(bot):
     await bot.add_cog(Info(bot))
