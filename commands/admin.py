7dea3fca9457d824ee04413588f2435b1d7d3d6b
Trevor Oler <18trevor3695@gmail.com>
2023-06-16 22:15:42 -0400

    Update admin.py

--- commands/admin.py
+++ commands/admin.py
@@ -6,7 +6,9 @@ import json
 from github import Github
 
 def get_config():
-    with open('../../config.json', 'r') as f:
+    parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    config_path = os.path.join(parent_dir, "config.json")
+    with open(config_path, "r") as f:
         config = json.load(f)
     return config
 
