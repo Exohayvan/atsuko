2332c91d92d0c40cecd985b6246e1425d64ebab3
Trevor Oler <18trevor3695@gmail.com>
2023-06-17 01:18:10 -0400

    Update music.py

--- commands/music.py
+++ commands/music.py
@@ -27,11 +27,11 @@ class Music(commands.Cog):
         if not ctx.voice_client:
             await ctx.send("I am not connected to a voice channel!")
             return
-        with ytdlp.YoutubeDL({'format': 'bestaudio/best'}) as ydl:
+        with ytdlp.YoutubeDL({'format': 'bestaudio/best', 'noplaylist':'True'}) as ydl:
             info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
+            URL = info['entries'][0]['url'] if 'entries' in info else info['url']
             voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-            voice.play(discord.FFmpegPCMAudio(url2, options="-vn"))
+            voice.play(discord.FFmpegPCMAudio(URL, options="-vn"))
             voice.is_playing()
 
     @commands.command()
