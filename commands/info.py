b9019ae8e47b346b84f810aa0dd9009b3f9cad58
Trevor Oler <18trevor3695@gmail.com>
2023-06-16 23:14:23 -0400

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -1,44 +1,52 @@
 from discord.ext import commands
 import datetime
+import asyncio
 
 class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.uptime_start = datetime.datetime.utcnow()  # The uptime should start from the moment of the latest bot's initialization
-        self.lifetime_start = self.load_lifetime_start()
+        self.uptime_start = datetime.datetime.utcnow()
+        self.total_uptime = self.load_total_uptime()
+        self.bot.loop.create_task(self.uptime_background_task())
 
-    def load_lifetime_start(self):
+    def load_total_uptime(self):
         try:
-            with open("start_time.txt", "r") as f:
-                start_time_str = f.read()
-                return datetime.datetime.fromisoformat(start_time_str)
+            with open("total_uptime.txt", "r") as f:
+                total_uptime_seconds = int(f.read())
+                return datetime.timedelta(seconds=total_uptime_seconds)
         except FileNotFoundError:
-            # If the file doesn't exist, that means it's the bot's first boot. 
-            # Therefore, we save the current boot time as the lifetime start.
-            self.save_lifetime_start()
-            return datetime.datetime.utcnow()
+            return datetime.timedelta(seconds=0)
 
-    def save_lifetime_start(self):
-        with open("start_time.txt", "w") as f:
-            lifetime_start_str = self.lifetime_start.isoformat()
-            f.write(lifetime_start_str)
+    def save_total_uptime(self):
+        total_uptime_seconds = int(self.total_uptime.total_seconds())
+        with open("total_uptime.txt", "w") as f:
+            f.write(str(total_uptime_seconds))
+
+    async def uptime_background_task(self):
+        while True:
+            current_uptime = datetime.datetime.utcnow() - self.uptime_start
+            self.total_uptime += current_uptime
+            self.save_total_uptime()
+            self.uptime_start = datetime.datetime.utcnow()
+            await asyncio.sleep(60)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        # When the bot is ready, update the uptime start.
         self.uptime_start = datetime.datetime.utcnow()
 
     @commands.command()
     async def uptime(self, ctx):
         """Shows the current uptime of the bot since last reboot."""
-        delta = datetime.datetime.utcnow() - self.uptime_start
-        await self.send_uptime_message(ctx, "Current Uptime", delta)
+        current_uptime = datetime.datetime.utcnow() - self.uptime_start
+        await self.send_uptime_message(ctx, "Current Uptime", current_uptime)
 
     @commands.command()
     async def lifetime(self, ctx):
         """Shows the total lifetime uptime of the bot."""
-        delta = datetime.datetime.utcnow() - self.lifetime_start
-        await self.send_uptime_message(ctx, "Total Lifetime Uptime", delta)
+        # Lifetime uptime includes current uptime
+        current_uptime = datetime.datetime.utcnow() - self.uptime_start
+        total_uptime = self.total_uptime + current_uptime
+        await self.send_uptime_message(ctx, "Total Lifetime Uptime", total_uptime)
 
     async def send_uptime_message(self, ctx, title, delta):
         days, seconds = delta.days, delta.seconds
