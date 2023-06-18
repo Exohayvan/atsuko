9006b512ae207b61358f0da16bc034ebbfe2c83e
Trevor Oler <18trevor3695@gmail.com>
2023-06-18 16:12:16 -0400

    Update voting.py

--- commands/voting.py
+++ commands/voting.py
@@ -1,12 +1,43 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 import asyncio
 import datetime
+import sqlite3
+import json
 
 class Voting(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.conn = sqlite3.connect('./data/voting.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS active_votes (
+                title TEXT PRIMARY KEY,
+                message_id INTEGER,
+                option_emojis TEXT,
+                votes TEXT,
+                end_time TEXT,
+                voted_users TEXT
+            )
+        """)
+        self.conn.commit()
         self.active_votes = {}
+        self.load_votes()
+
+    def cog_unload(self):
+        self.conn.close()
+
+    def load_votes(self):
+        self.cursor.execute("SELECT * FROM active_votes")
+        for row in self.cursor.fetchall():
+            title, message_id, option_emojis, votes, end_time, voted_users = row
+            self.active_votes[title] = {
+                'message_id': message_id,
+                'option_emojis': json.loads(option_emojis),
+                'votes': json.loads(votes),
+                'end_time': datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S.%f"),
+                'voted_users': json.loads(voted_users),
+            }
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -20,7 +51,6 @@ class Voting(commands.Cog):
                 if emoji in vote_data['option_emojis']:
                     option = vote_data['option_emojis'][emoji]
 
-                    # If the user has already voted for a different option, remove their old vote
                     if user.id in vote_data['voted_users']:
                         old_option = vote_data['voted_users'][user.id]
                         if old_option != option:
@@ -28,10 +58,9 @@ class Voting(commands.Cog):
 
                     vote_data['votes'][option] += 1
                     vote_data['voted_users'][user.id] = option
-                
-                    # DM the user to confirm their vote
+
                     await user.send(f"Your vote for '{title}' has been changed to: {option}")
-                
+
                     await message.remove_reaction(reaction.emoji, user)
                 break
 
@@ -70,20 +99,17 @@ class Voting(commands.Cog):
         time_name, multiplier = time_formats[unit]
         end_time = datetime.datetime.utcnow() + datetime.timedelta(**{time_name: amount * multiplier})
 
-        # Create and send an embedded message with voting details
         embed = discord.Embed(title=f"Vote: {title}", description="React with the number emojis to vote. Votes are anonymous and reaction will disappear after voting. Only last selection will be tracked. Votes after vote ends, will not count.")
         option_emojis = {f"{i+1}\u20e3": option for i, option in enumerate(options)}
         for i, option in enumerate(options):
-            emoji = f"{i+1}\u20e3"  # Generate number emojis
+            emoji = f"{i+1}\u20e3"  
             embed.add_field(name=f"Option {i+1}", value=f"{option} {emoji}", inline=False)
         voting_message = await ctx.send(embed=embed)
 
-        # React with corresponding emojis
         for i in range(len(options)):
-            emoji = f"{i+1}\u20e3"  # Generate number emojis
+            emoji = f"{i+1}\u20e3"  
             await voting_message.add_reaction(emoji)
 
-        # Add the message_id, option_emojis, votes, and voted_users to the active_votes data
         self.active_votes[title] = {
             'message_id': voting_message.id,
             'option_emojis': option_emojis,
@@ -91,9 +117,13 @@ class Voting(commands.Cog):
             'end_time': end_time,
             'voted_users': {}
         }
+        
+        self.cursor.execute("INSERT INTO active_votes VALUES (?, ?, ?, ?, ?, ?)",
+                            (title, voting_message.id, json.dumps(option_emojis), json.dumps({option: 0 for option in options}),
+                            str(end_time), json.dumps({})))
+        self.conn.commit()
 
         while datetime.datetime.utcnow() < end_time:
-            # Update the vote counts based on reactions
             updated_voting_message = await ctx.fetch_message(voting_message.id)
             for reaction in updated_voting_message.reactions:
                 if str(reaction.emoji) in option_emojis:
@@ -108,17 +138,15 @@ class Voting(commands.Cog):
         updated_voting_message = await ctx.fetch_message(voting_message.id)
         embed.set_footer(text="Voting Ended")
 
-        # Determine the winning option(s)
         max_votes = max(self.active_votes[title]['votes'].values())
         winning_options = [option for option, votes in self.active_votes[title]['votes'].items() if votes == max_votes]
 
-        # Create an embedded message with the winning option(s)
         winning_embed = discord.Embed(title="Voting Results", description="The vote has ended. Here are the winning option(s):")
         for i, option in enumerate(winning_options):
             winning_embed.add_field(name=f"Option {i+1}", value=option, inline=False)
 
         await ctx.send(embed=winning_embed)
-        del self.active_votes[title]  # Clean up after the vote is finished
+        del self.active_votes[title]
 
 async def setup(bot):
-    await bot.add_cog(Voting(bot))
+    bot.add_cog(Voting(bot))
