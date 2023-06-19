3192992b7ddc6e120642e736e9c53f6e6de19657
Trevor Oler <18trevor3695@gmail.com>
2023-06-19 00:52:44 -0400

    Update family.py

--- commands/family.py
+++ commands/family.py
@@ -8,7 +8,7 @@ class Family(commands.Cog):
         self.conn = self.create_connection()
 
     def create_connection(self):
-        conn = None;
+        conn = None
         try:
             conn = sqlite3.connect('./data/family.db') 
             print(sqlite3.version)
@@ -19,12 +19,73 @@ class Family(commands.Cog):
     @commands.command()
     async def adopt(self, ctx, *, member: discord.Member):
         """Sends an adoption request to another member."""
-        await ctx.send(f"{ctx.author.mention} has sent an adoption request to {member.mention}.")
+        if ctx.author == member:
+            await ctx.send(f"{ctx.author.mention}, you cannot send an adoption request to yourself.")
+            return
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("INSERT INTO AdoptionRequests(sender_id, receiver_id) VALUES (?, ?)", (ctx.author.id, member.id))
+            self.conn.commit()
+            await ctx.send(f"{ctx.author.mention} has sent an adoption request to {member.mention}.")
+        except Error as e:
+            print(e)
 
     @commands.command()
     async def accept_adoption(self, ctx):
         """Accepts an adoption request."""
-        await ctx.send(f"{ctx.author.mention} has accepted the adoption request.")
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM AdoptionRequests WHERE receiver_id = ?", (ctx.author.id,))
+            if cursor.fetchone() is None:
+                await ctx.send(f"No adoption requests for {ctx.author.mention}.")
+                return
+            cursor.execute("UPDATE AdoptionRequests SET accepted = 1 WHERE receiver_id = ?", (ctx.author.id,))
+            cursor.execute("DELETE FROM AdoptionRequests WHERE accepted = 1 AND receiver_id = ?", (ctx.author.id,))
+            self.conn.commit()
+            await ctx.send(f"{ctx.author.mention} has accepted the adoption request.")
+        except Error as e:
+            print(e)
+
+    @commands.command()
+    async def reject_adoption(self, ctx):
+        """Rejects an adoption request."""
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("DELETE FROM AdoptionRequests WHERE receiver_id = ?", (ctx.author.id,))
+            self.conn.commit()
+            await ctx.send(f"{ctx.author.mention} has rejected the adoption request.")
+        except Error as e:
+            print(e)
+
+    @commands.command()
+    async def propose(self, ctx, *, member: discord.Member):
+        """Sends a marriage proposal to another member."""
+        if ctx.author == member:
+            await ctx.send(f"{ctx.author.mention}, you cannot send a marriage proposal to yourself.")
+            return
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("INSERT INTO MarriageRequests(sender_id, receiver_id) VALUES (?, ?)", (ctx.author.id, member.id))
+            self.conn.commit()
+            await ctx.send(f"{ctx.author.mention} has sent a marriage proposal to {member.mention}.")
+        except Error as e:
+            print(e)
+
+    @commands.command()
+    async def accept_marriage(self, ctx):
+        """Accepts a marriage proposal."""
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM MarriageRequests WHERE receiver_id = ?", (ctx.author.id,))
+            if cursor.fetchone() is None:
+                await ctx.send(f"No marriage proposals for {ctx.author.mention}.")
+                return
+            cursor.execute("UPDATE MarriageRequests SET accepted = 1 WHERE receiver_id = ?", (ctx.author.id,))
+            cursor.execute("DELETE FROM MarriageRequests WHERE accepted = 1 AND receiver_id = ?", (ctx.author.id,))
+            self.conn.commit()
+            await ctx.send(f"{ctx.author.mention} has accepted the marriage proposal.")
+        except Error as e:
+            print(e)
 
     @commands.command()
     async def show_family(self, ctx):
