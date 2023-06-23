5459d9b0ec75dac6bcbf6bd036b90c17620858ba
Trevor Oler <18trevor3695@gmail.com>
2023-06-23 12:47:21 -0400

    Update command_usage.py

--- commands/usage_trackers/command_usage.py
+++ commands/usage_trackers/command_usage.py
@@ -5,7 +5,7 @@ import os
 
 # Import the OwnerCommands cog
 # Adjust the import statement according to your project structure.
-from commands.Owner import OwnerCommands
+from commands.owner import OwnerCommands
 
 class CommandUsageTracker(commands.Cog):
     def __init__(self, bot):
