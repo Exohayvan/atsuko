d2ab8da46e53df31e52c618492d94efca9cc44fb
Trevor Oler <18trevor3695@gmail.com>
2023-06-23 21:41:03 -0400

    Update money.py

--- commands/economy/money.py
+++ commands/economy/money.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 # Define these at the top of your script
 MAX_AMT = 250
 MIN_AMT = 50
+ZERO_AMT_CHANCE = 20  # Chance of receiving zero gold
 CURRENCY_NAME = "gold"
 
 class Money(commands.Cog):
@@ -48,9 +49,7 @@ class Money(commands.Cog):
             await ctx.send('You already received your daily gold. Please wait until tomorrow.')
             return
 
-        gold_gain = 0
-        if random.randint(1, 100) <= 80:
-            gold_gain = random.randint(MIN_AMT, MAX_AMT)
+        gold_gain = random.randint(MIN_AMT, MAX_AMT) if random.randint(1, 100) > ZERO_AMT_CHANCE else 0
 
         if result:
             self.cursor.execute('UPDATE UserBalance SET balance=balance+?, last_daily=? WHERE user_id=?', (gold_gain, datetime.now(), user_id))
@@ -59,6 +58,33 @@ class Money(commands.Cog):
 
         self.db.commit()
         await ctx.send(f"You received {gold_gain} {CURRENCY_NAME}.")
+
+    @commands.command()
+    async def give(self, ctx, member: commands.MemberConverter, amount: int):
+        """Give gold to someone."""
+        user_id = str(ctx.author.id)
+        target_id = str(member.id)
+
+        # Check if the user has enough gold to give
+        self.cursor.execute('SELECT balance FROM UserBalance WHERE user_id=?', (user_id,))
+        result = self.cursor.fetchone()
+        if result is None or result[0] < amount:
+            await ctx.send("You don't have enough gold to give.")
+            return
+
+        # Subtract gold from the giver
+        self.cursor.execute('UPDATE UserBalance SET balance=balance-? WHERE user_id=?', (amount, user_id))
+
+        # Add gold to the receiver
+        self.cursor.execute('SELECT balance FROM UserBalance WHERE user_id=?', (target_id,))
+        result = self.cursor.fetchone()
+        if result:
+            self.cursor.execute('UPDATE UserBalance SET balance=balance+? WHERE user_id=?', (amount, target_id))
+        else:
+            self.cursor.execute('INSERT INTO UserBalance (user_id, balance) VALUES (?, ?)', (target_id, amount))
+
+        self.db.commit()
+        await ctx.send(f"You gave {amount} {CURRENCY_NAME} to {member.mention}.")
 
 async def setup(bot):
     await bot.add_cog(Money(bot))
