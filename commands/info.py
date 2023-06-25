1a858d8a2bb8698cf0b0244b92fa9747e415775b
Trevor Oler <18trevor3695@gmail.com>
2023-06-25 11:08:31 -0400

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -219,14 +219,14 @@ class Info(commands.Cog):
     async def uptime(self, ctx):
         """Shows the current uptime of the bot since last restart."""
         current_uptime = datetime.datetime.utcnow() - self.bot_start_time
-        lifetime_uptime = self.get_total_uptime() + current_uptime
+        total_uptime = self.total_uptime + current_uptime
         uptime_last_30_days = self.get_uptime_for_last_30_days()
         total_seconds_last_30_days = 30 * 24 * 60 * 60
         uptime_percentage_last_30_days = (uptime_last_30_days.total_seconds() / total_seconds_last_30_days) * 100
     
         embed = discord.Embed(title="Current Uptime", color=discord.Color.blue())
         embed.add_field(name="Since Last Restart", value=self.format_timedelta(current_uptime), inline=False)
-        embed.add_field(name="Lifetime", value=self.format_timedelta(lifetime_uptime), inline=False)
+        embed.add_field(name="Lifetime", value=self.format_timedelta(total_uptime), inline=False)
         embed.add_field(name="Last 30 Days", value=f"{uptime_percentage_last_30_days:.2f}% of total time", inline=False)
     
         await ctx.send(embed=embed)
