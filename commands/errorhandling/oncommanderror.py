7c224d1a02568b65e835fa39592aeb1efd78a02f
Trevor Oler <18trevor3695@gmail.com>
2023-06-27 10:41:46 -0400

    Update oncommanderror.py

--- commands/errorhandling/oncommanderror.py
+++ commands/errorhandling/oncommanderror.py
@@ -3,7 +3,7 @@ import json
 from github import Github
 
 def get_config():
-    with open('../../../../config.json', 'r') as f:
+    with open('../config.json', 'r') as f:
         config = json.load(f)
     print(config)
     return config
