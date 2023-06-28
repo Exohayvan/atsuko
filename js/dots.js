3456382fb54ea1aee2f15585f3493eebd6a60a90
Trevor Oler <18trevor3695@gmail.com>
2023-06-28 01:24:37 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -1,16 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var numDots = 200; // Increase the number of dots for better coverage
+    var numDots = 200;  // Increase this if you want more dots
     var bodyRect = document.body.getBoundingClientRect();
+    var directions = ['top', 'right', 'bottom', 'left'];
 
     for (var i = 0; i < numDots; i++) {
         var dot = document.createElement("div");
+        var direction = directions[Math.floor(Math.random() * directions.length)];
         dot.classList.add("dot");
-        dot.style.top = Math.floor(Math.random() * bodyRect.height) + "px";
-        dot.style.left = Math.floor(Math.random() * bodyRect.width) + "px";
-        // Assign random ending points
-        dot.style.setProperty('--end-left', Math.floor(Math.random() * bodyRect.width) + "px");
-        dot.style.setProperty('--end-top', Math.floor(Math.random() * bodyRect.height) + "px");
-        dot.style.animationDuration = Math.random() * 10 + "s";
+        dot.classList.add(direction);
+        switch (direction) {
+            case 'top':
+                dot.style.top = "0px";
+                dot.style.left = Math.random() * bodyRect.width + "px";
+                break;
+            case 'right':
+                dot.style.top = Math.random() * bodyRect.height + "px";
+                dot.style.right = "0px";
+                break;
+            case 'bottom':
+                dot.style.bottom = "0px";
+                dot.style.left = Math.random() * bodyRect.width + "px";
+                break;
+            case 'left':
+                dot.style.top = Math.random() * bodyRect.height + "px";
+                dot.style.left = "0px";
+                break;
+        }
+        dot.style.animationDuration = (Math.random() * 5 + 5) + "s";
         document.body.appendChild(dot);
     }
 });
