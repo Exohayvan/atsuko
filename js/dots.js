8adbe2e491957a5ff072238ea7db21c6ac643b4b
Trevor Oler <18trevor3695@gmail.com>
2023-06-28 01:13:52 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -1,26 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var numDots = 50;
+    var numDots = 100; // Increasing the number of dots
     var bodyRect = document.body.getBoundingClientRect();
-
-    document.body.setAttribute("data-width", bodyRect.width);
-    document.body.setAttribute("data-height", bodyRect.height);
-
     var diagonal = Math.sqrt(bodyRect.width**2 + bodyRect.height**2);
 
     for (var i = 0; i < numDots; i++) {
         var dot = document.createElement("div");
         dot.classList.add("dot");
         dot.style.top = Math.floor(Math.random() * bodyRect.height) + "px";
-        dot.style.left = "0px";
-        dot.style.animationDuration = (diagonal / (Math.random() * 50 + 50)) + "s";
-        document.body.appendChild(dot);
-    }
-
-    for (var i = 0; i < numDots; i++) {
-        var dot = document.createElement("div");
-        dot.classList.add("dot");
-        dot.style.top = "0px";
-        dot.style.left = Math.floor(Math.random() * bodyRect.width) + "px";
+        dot.style.left = Math.floor(Math.random() * bodyRect.width) + "px"; // Random position for left and top
         dot.style.animationDuration = (diagonal / (Math.random() * 50 + 50)) + "s";
         document.body.appendChild(dot);
     }
