9460f790ab3697541eff63b4aac1e22f832194a0
Trevor Oler <18trevor3695@gmail.com>
2023-06-28 02:01:45 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     var wrapper = document.getElementById('dot-wrapper');
     var wrapperRect = wrapper.getBoundingClientRect();
-    var directions = ['left', 'right'];
 
     function createDot() {
         var dot = document.createElement("div");
-        var direction = directions[Math.floor(Math.random() * directions.length)];
         dot.classList.add("dot");
-        dot.classList.add(direction);
         dot.style.top = Math.floor(Math.random() * wrapperRect.height) + "px";
-        dot.style.animationDuration = Math.random() * 5 + 5 + "s";
+        dot.style.left = Math.floor(Math.random() * wrapperRect.width) + "px";
+        dot.style.animationDuration = (Math.random() * 5 + 5) + "s";
         wrapper.appendChild(dot);
 
         // Remove dot after it finishes animating
