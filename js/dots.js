9fd1ac06513c17a308c43698e2225fe0dcfd0a5a
Trevor Oler <18trevor3695@gmail.com>
2023-06-28 01:05:51 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -1,27 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     var numDots = 50;
     var bodyRect = document.body.getBoundingClientRect();
-    var dotSize = 10;
 
     for (var i = 0; i < numDots; i++) {
         var dot = document.createElement("div");
         dot.classList.add("dot");
-        dot.style.top = 0 + "px";
-        dot.style.left = getRandomPosition(0, bodyRect.width - dotSize) + "px";
-        dot.style.animationDuration = Math.random() * 10 + "s";
+        dot.style.top = Math.floor(Math.random() * bodyRect.height) + "px";
+        dot.style.left = "0px";
+        dot.style.animationDuration = (Math.random() * 5 + 5) + "s";
         document.body.appendChild(dot);
     }
 
     for (var i = 0; i < numDots; i++) {
         var dot = document.createElement("div");
         dot.classList.add("dot");
-        dot.style.left = 0 + "px";
-        dot.style.top = getRandomPosition(0, bodyRect.height - dotSize) + "px";
-        dot.style.animationDuration = Math.random() * 10 + "s";
+        dot.style.top = "0px";
+        dot.style.left = Math.floor(Math.random() * bodyRect.width) + "px";
+        dot.style.animationDuration = (Math.random() * 5 + 5) + "s";
         document.body.appendChild(dot);
     }
 });
-
-function getRandomPosition(min, max) {
-    return Math.random() * (max - min) + min;
-}
