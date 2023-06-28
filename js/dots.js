783b35d2956274d45c500f443ba778657195e095
Trevor Oler <18trevor3695@gmail.com>
2023-06-28 01:30:41 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -1,32 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var numDots = 200;  // Increase this if you want more dots
+    var numDots = 1000;  // Increase this if you want more dots
     var bodyRect = document.body.getBoundingClientRect();
-    var directions = ['top', 'right', 'bottom', 'left'];
 
     for (var i = 0; i < numDots; i++) {
         var dot = document.createElement("div");
-        var direction = directions[Math.floor(Math.random() * directions.length)];
         dot.classList.add("dot");
-        dot.classList.add(direction);
-        switch (direction) {
-            case 'top':
-                dot.style.top = "0px";
-                dot.style.left = Math.random() * bodyRect.width + "px";
-                break;
-            case 'right':
-                dot.style.top = Math.random() * bodyRect.height + "px";
-                dot.style.right = "0px";
-                break;
-            case 'bottom':
-                dot.style.bottom = "0px";
-                dot.style.left = Math.random() * bodyRect.width + "px";
-                break;
-            case 'left':
-                dot.style.top = Math.random() * bodyRect.height + "px";
-                dot.style.left = "0px";
-                break;
-        }
-        dot.style.animationDuration = (5) + "s";
+        dot.style.top = Math.floor(Math.random() * bodyRect.height) + "px"; // Initial position randomly set within the body height
+        dot.style.left = Math.floor(Math.random() * bodyRect.width) + "px"; // Initial position randomly set within the body width
+        dot.style.animationDuration = (Math.random() * 5 + 5) + "s"; // Random animation duration between 5 to 10 seconds
         document.body.appendChild(dot);
     }
 });
