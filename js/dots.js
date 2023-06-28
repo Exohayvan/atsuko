ce31bed63d901ab894f3d7ed437f75810c8e401e
Trevor Oler <18trevor3695@gmail.com>
2023-06-28 00:58:59 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -7,8 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
         var dot = document.createElement("div");
         dot.classList.add("dot");
         dot.style.top = getRandomPosition(0, bodyRect.height - dotSize) + "px";
+        dot.style.left = 0 + "px"; // Set initial position to be along the left edge
+        dot.style.animationDuration = Math.random() * 10 + "s"; // You can adjust animation duration as per your needs.
+        document.body.appendChild(dot);
+    }
+
+    for (var i = 0; i < numDots; i++) {
+        var dot = document.createElement("div");
+        dot.classList.add("dot");
         dot.style.left = getRandomPosition(0, bodyRect.width - dotSize) + "px";
-        dot.style.animationDuration = Math.max(bodyRect.width, bodyRect.height)/100 + "s"; // Adjusted the animation duration according to the screen size.
+        dot.style.top = 0 + "px"; // Set initial position to be along the top edge
+        dot.style.animationDuration = Math.random() * 10 + "s"; // You can adjust animation duration as per your needs.
         document.body.appendChild(dot);
     }
 });
