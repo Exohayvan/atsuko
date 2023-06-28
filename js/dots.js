8462038fd819a932e8ddb66d859156b8c453377b
Trevor Oler <18trevor3695@gmail.com>
2023-06-27 22:50:11 -0400

    Update dots.js

--- js/dots.js
+++ js/dots.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var numDots = 50;
-    var container = document.querySelector(".container");
-    var containerRect = container.getBoundingClientRect();
-  
+    var dotsContainer = document.createElement("div");
+    dotsContainer.classList.add("dots-container");
+    document.body.appendChild(dotsContainer);
+    var containerRect = document.querySelector(".container").getBoundingClientRect();
+
     for (var i = 0; i < numDots; i++) {
         var dot = document.createElement("div");
         dot.classList.add("dot");
         dot.style.top = Math.random() * containerRect.height + "px";
         dot.style.left = Math.random() * containerRect.width + "px";
         dot.style.animationDelay = Math.random() * 10 + "s";
-        container.appendChild(dot);
+        dotsContainer.appendChild(dot);
     }
 });
