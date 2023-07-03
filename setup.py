db6db9230afe3ef3cd9aef4f7e2a08178057cc95
Trevor Oler <18trevor3695@gmail.com>
2023-07-03 18:04:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import json
 import os
 
-# Prompt for the bot token and GitHub token
+# Prompt for the bot token, GitHub token, and private key path
 bot_token = input("Please enter your bot token: ")
 github_token = input("Please enter your GitHub token: ")
+private_key_path = input("Please enter the private key path: ")
 
 # Hardcode the owner ID
 owner_id = 276782057412362241
@@ -12,11 +13,33 @@ owner_id = 276782057412362241
 config = {
     "bot_token": bot_token,
     "GITHUB_TOKEN": github_token,
-    "owner_id": owner_id
+    "owner_id": owner_id,
+    "PRIVATE_KEY_PATH": private_key_path
 }
 
-# Write the dictionary to a JSON file
-with open('../config.json', 'w') as f:
-    json.dump(config, f)
+# Check if the JSON file exists
+if os.path.exists('../config.json'):
+    # Read the existing JSON file
+    with open('../config.json', 'r') as f:
+        existing_config = json.load(f)
+    
+    # Check if each key exists in the existing config
+    for key in config:
+        if key in existing_config:
+            # Ask if the value should be updated
+            update = input(f"The {key} already exists. Do you want to update it? (y/n): ")
+            if update.lower() == 'y':
+                existing_config[key] = config[key]
+        else:
+            # Key doesn't exist, add it to the existing config
+            existing_config[key] = config[key]
+
+    # Write the updated config to the JSON file
+    with open('../config.json', 'w') as f:
+        json.dump(existing_config, f)
+else:
+    # Write the new config to a JSON file
+    with open('../config.json', 'w') as f:
+        json.dump(config, f)
 
 print("Config saved to ../config.json")
