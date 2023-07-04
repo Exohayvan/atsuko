fe813b3cac875bf2d78e61b5fecf8f92678ad2e7
Trevor Oler <18trevor3695@gmail.com>
2023-07-04 19:50:26 -0400

    Create presence.py

---
+++ commands/statuses/presence.py
@@ -0,0 +1,33 @@
+from discord.ext import commands, tasks
+from discord import Game, ActivityType
+import itertools
+
+class Presence(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.change_presence.start()  # Start the task
+
+    def cog_unload(self):
+        self.change_presence.cancel()  # Cancel the task when the cog gets unloaded
+
+    @tasks.loop(seconds=30)
+    async def change_presence(self):
+        """Automatically changes the bot's presence every 30 seconds."""
+        statuses = [
+            "playing with the !help command 📚",  
+            f"playing with {len(self.bot.users)} users 👥",
+            f"playing on {len(self.bot.guilds)} servers 🌐",
+            f"playing around {sum(len(guild.channels) for guild in self.bot.guilds)} channels 💬",
+            "playing with my creator, ExoHayvan 🛠️"
+        ]
+        self.statuses = itertools.cycle(statuses)
+        next_status = next(self.statuses)
+        await self.bot.change_presence(activity=Game(name=next_status))
+        print(f'Presence changed to: {next_status}')
+
+    @change_presence.before_loop
+    async def before_change_presence(self):
+        await self.bot.wait_until_ready()  # Wait until the bot has connected to discord
+
+async def setup(bot):
+    await bot.add_cog(Presence(bot))
