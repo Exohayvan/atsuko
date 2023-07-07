05651b28d0465d1d33a272e47a2c7e1196a1e615
Trevor Oler <18trevor3695@gmail.com>
2023-07-06 21:48:39 -0400

    Create feedback.py

---
+++ commands/feedback/feedback.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+from github import GithubIntegration
+import github
+import json
+import os
+
+def get_config():
+    with open('../config.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+class Feedback(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = get_config()
+        self.github_repo = "Exohayvan/atsuko"  # Hardcode the repo here
+        self.app_id = self.config.get('APP_ID')
+        self.installation_id = self.config.get('INSTALLATION_ID')
+        self.private_key_path = self.config.get('PRIVATE_KEY_PATH')
+
+    @commands.command(name='feedback')
+    async def feedback(self, ctx, *, message):
+        issue_title = f"User Feedback: {ctx.author} - {ctx.message.id}"
+        issue_body = (f"**User Message:** {message}\n"
+                      f"**Author:** {ctx.author}\n"
+                      f"**Channel:** {ctx.channel}")
+
+        private_key = open(self.private_key_path, 'r').read()
+        integration = GithubIntegration(self.app_id, private_key)
+        token = integration.get_access_token(self.installation_id)
+
+        try:
+            g = github.Github(token.token)
+            repo = g.get_repo(self.github_repo)
+            issue = repo.create_issue(title=issue_title, body=issue_body)
+
+            embed = discord.Embed(title='Feedback received', color=0x00ff00)
+            embed.add_field(name='Issue created on GitHub', value=f'[Link to issue]({issue.html_url})', inline=False)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"I am unable to open an issue on GitHub.")
+            await ctx.send(f"An unexpected error occurred: {e}")
+
+async def setup(bot):
+    await bot.add_cog(Feedback(bot))
