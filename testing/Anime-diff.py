ce8ad3b66f7e8b053f5b0e44ba62dd7387927fd4
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 02:22:47 -0400

    Update Anime-diff.py

--- testing/Anime-diff.py
+++ testing/Anime-diff.py
@@ -6,7 +6,7 @@ model_id = "dreamlike-art/dreamlike-anime-1.0"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
 # Take user input for the prompt
-prompt = input("Enter the prompt: ")
+prompt = "anime, masterpiece, high quality, high resolution " + input("Enter the prompt: ")
 
 negative_prompt = 'simple background, duplicate, retro style, low quality, lowest quality, 1980s, 1990s, 2000s, 2005 2006 2007 2008 2009 2010 2011 2012 2013, bad anatomy, bad proportions, extra digits, lowres, username, artist name, error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry'
 image = pipe(prompt, negative_prompt=negative_prompt).images[0]
