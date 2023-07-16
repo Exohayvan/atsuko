07fa9c71236e2cc590b44d2ac7e8c837d602293a
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 01:31:15 -0400

    Update Anime-diff.py

--- testing/Anime-diff.py
+++ testing/Anime-diff.py
@@ -1,9 +1,17 @@
 from diffusers import StableDiffusionPipeline
+import hashlib
 
 model_id = "Linaqruf/anything-v3.0"
 pipe = StableDiffusionPipeline.from_pretrained(model_id)
 
-prompt = "pikachu"
+# Take user input for the prompt
+prompt = input("Enter the prompt: ")
+
 image = pipe(prompt).images[0]
 
-image.save("./pikachu.png")
+# Generate the MD5 hash of the image data
+hash_object = hashlib.md5(image.tobytes())
+filename = hash_object.hexdigest() + ".png"
+
+# Save the image with the hashed filename
+image.save("./" + filename)
