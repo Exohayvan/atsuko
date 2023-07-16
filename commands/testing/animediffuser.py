683abd60625881a754a32e6791ae8ef0f051aa15
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 01:46:39 -0400

    Update animediffuser.py

--- commands/testing/animediffuser.py
+++ commands/testing/animediffuser.py
@@ -1,6 +1,8 @@
 from diffusers import StableDiffusionPipeline
 import hashlib
+import asyncio
 from discord.ext import commands
+import discord
 
 class AnimeDiff(commands.Cog):
     def __init__(self, bot):
@@ -11,7 +13,8 @@ class AnimeDiff(commands.Cog):
     @commands.command()
     async def animediff(self, ctx, *, prompt_input):
         """Generates an image using the AnimeDiff model."""
-        image = self.pipe(prompt_input).images[0]
+        loop = asyncio.get_event_loop()
+        image = await loop.run_in_executor(None, self.generate_image, prompt_input)
 
         # Generate the MD5 hash of the image data
         hash_object = hashlib.md5(image.tobytes())
@@ -21,6 +24,9 @@ class AnimeDiff(commands.Cog):
         image.save("./" + filename)
 
         await ctx.send(file=discord.File(filename))
+
+    def generate_image(self, prompt_input):
+        return self.pipe(prompt_input).images[0]
 
 async def setup(bot):
     await bot.add_cog(AnimeDiff(bot))
