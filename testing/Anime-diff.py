fb265fe73fe07e355cad9572e558d151f503a44b
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 02:01:24 -0400

    Update Anime-diff.py

--- testing/Anime-diff.py
+++ testing/Anime-diff.py
@@ -1,7 +1,7 @@
 from diffusers import StableDiffusionPipeline
 import hashlib
 
-model_id = "yangheng/Super-Resolution-Anime-Diffusion"
+model_id = "Linaqruf/anything-v3.0"
 pipe = StableDiffusionPipeline.from_pretrained(model_id)
 
 # Take user input for the prompt
