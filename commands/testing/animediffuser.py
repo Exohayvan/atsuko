b56e4a5048c63c614dd5a684e4806806a06a15e3
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 02:45:08 -0400

    Update animediffuser.py

--- commands/testing/animediffuser.py
+++ commands/testing/animediffuser.py
@@ -1,20 +1,22 @@
 from diffusers import StableDiffusionPipeline
 import hashlib
-import asyncio
+import torch
 from discord.ext import commands
 import discord
 
 class AnimeDiff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.model_id = "Linaqruf/anything-v3.0"
-        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id)
+        self.model_id = "dreamlike-art/dreamlike-anime-1.0"
+        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float32)
 
     @commands.command()
     async def animediff(self, ctx, *, prompt_input):
         """Generates an image using the AnimeDiff model."""
-        loop = asyncio.get_event_loop()
-        image = await loop.run_in_executor(None, self.generate_image, prompt_input)
+        prompt = "anime, masterpiece, high quality, high resolution " + prompt_input
+        negative_prompt = 'simple background, duplicate, retro style, low quality, lowest quality, 1980s, 1990s, 2000s, 2005 2006 2007 2008 2009 2010 2011 2012 2013, bad anatomy, bad proportions, extra digits, lowres, username, artist name, error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry'
+
+        image = await self.generate_image(prompt, negative_prompt)
 
         # Generate the MD5 hash of the image data
         hash_object = hashlib.md5(image.tobytes())
@@ -25,8 +27,8 @@ class AnimeDiff(commands.Cog):
 
         await ctx.send(file=discord.File(filename))
 
-    def generate_image(self, prompt_input):
-        return self.pipe(prompt_input).images[0]
+    async def generate_image(self, prompt, negative_prompt):
+        return await self.bot.loop.run_in_executor(None, self.pipe, prompt, negative_prompt)
 
 async def setup(bot):
     await bot.add_cog(AnimeDiff(bot))
