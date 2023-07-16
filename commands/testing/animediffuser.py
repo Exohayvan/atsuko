af572decfc3925cd6696a095669c38aa777e40fe
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 03:02:04 -0400

    Update animediffuser.py

--- commands/testing/animediffuser.py
+++ commands/testing/animediffuser.py
@@ -1,44 +1,39 @@
-from diffusers import StableDiffusionPipeline
-import hashlib
-import torch
-import asyncio
 from discord.ext import commands
 import discord
+from diffusers import StableDiffusionPipeline
+import torch
+import hashlib
+import random
+import asyncio
 
-class AnimeDiff(commands.Cog):
+class ImageGenerator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.model_id = "dreamlike-art/dreamlike-anime-1.0"
         self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float32)
-        self.timeout = 1200  # 20 minutes (in seconds)
+        self.negative_prompt = 'simple background, duplicate, retro style, low quality, lowest quality, 1980s, 1990s, 2000s, 2005 2006 2007 2008 2009 2010 2011 2012 2013, bad anatomy, bad proportions, extra digits, lowres, username, artist name, error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry'
 
     @commands.command()
-    async def animediff(self, ctx, *, prompt_input):
-        """Generates an image using the AnimeDiff model."""
-        prompt = "anime, masterpiece, high quality, high resolution " + prompt_input
-        negative_prompt = 'simple background, duplicate, retro style, low quality, lowest quality, 1980s, 1990s, 2000s, 2005 2006 2007 2008 2009 2010 2011 2012 2013, bad anatomy, bad proportions, extra digits, lowres, username, artist name, error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry'
+    async def generate_image(self, ctx, prompt: str):
+        """Generates an image based on the provided prompt and sends the MD5 hash of the image data."""
 
-        # Send initial message indicating image generation
-        generating_msg = await ctx.send("Generating image... This process may take 10-20 minutes. Please wait.")
+        def generate_and_save_image():
+            prompt = "anime, masterpiece, high quality, high resolution " + prompt
+            image = self.pipe(prompt, negative_prompt=self.negative_prompt).images[0]
 
-        try:
-            image = await asyncio.wait_for(self.generate_image(prompt, negative_prompt), timeout=self.timeout)
-        except asyncio.TimeoutError:
-            await generating_msg.edit(content="Image generation process took too long. Please try again later.")
-            return
+            # Generate the MD5 hash of the image data
+            hash_object = hashlib.md5(image.tobytes())
+            filename = hash_object.hexdigest() + ".png"
 
-        # Generate the MD5 hash of the image data
-        hash_object = hashlib.md5(image.tobytes())
-        filename = hash_object.hexdigest() + ".png"
+            # Save the image with the hashed filename
+            image.save("./" + filename)
 
-        # Save the image with the hashed filename
-        image.save("./" + filename)
+            return filename
 
-        await ctx.send(file=discord.File(filename))
-        await generating_msg.delete()
+        filename = await asyncio.to_thread(generate_and_save_image)
 
-    async def generate_image(self, prompt, negative_prompt):
-        return await self.bot.loop.run_in_executor(None, self.pipe, prompt, negative_prompt)
+        # Send the hash to the user
+        await ctx.send(f"The MD5 hash of your image is: {filename[:-4]}")
 
 async def setup(bot):
-    await bot.add_cog(AnimeDiff(bot))
+    await bot.add_cog(ImageGenerator(bot))
