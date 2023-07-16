e0cd650a9d77b7e71a0175e3650d805f9997aca3
Trevor Oler <18trevor3695@gmail.com>
2023-07-16 02:16:10 -0400

    Update Anime-diff.py

--- testing/Anime-diff.py
+++ testing/Anime-diff.py
@@ -1,13 +1,14 @@
 from diffusers import StableDiffusionPipeline
 import hashlib
 
-model_id = "Linaqruf/anything-v3.0"
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
+model_id = "dreamlike-art/dreamlike-anime-1.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
 # Take user input for the prompt
 prompt = input("Enter the prompt: ")
 
-image = pipe(prompt).images[0]
+negative_prompt = 'simple background, duplicate, retro style, low quality, lowest quality, 1980s, 1990s, 2000s, 2005 2006 2007 2008 2009 2010 2011 2012 2013, bad anatomy, bad proportions, extra digits, lowres, username, artist name, error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry'
+image = pipe(prompt, negative_prompt=negative_prompt).images[0]
 
 # Generate the MD5 hash of the image data
 hash_object = hashlib.md5(image.tobytes())
