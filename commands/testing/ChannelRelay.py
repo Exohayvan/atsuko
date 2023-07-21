809638b0b8894bdb323ba9212adbfa1598fa9337
Trevor Oler <18trevor3695@gmail.com>
2023-07-21 00:10:32 -0400

    Update ChannelRelay.py

--- commands/testing/ChannelRelay.py
+++ commands/testing/ChannelRelay.py
@@ -1,12 +1,16 @@
 import discord
 from discord.ext import commands, tasks
 import datetime
+import sqlite3
+
+DATABASE_PATH = './data/db/channelrelays.db'
 
 class ChannelRelay(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.connected_channels = set()
-        self.last_message_times = {}  # Stores the last time a message was sent in a channel: {channel_id: datetime.datetime}
+        self.setup_database()
+        self.connected_channels = self.load_channels_from_db()
+        self.last_message_times = self.load_last_message_times_from_db()
         self.safety_message = (
             "🔒 **Online Safety Reminder** 🔒\n\n"
             "- Always be cautious when sharing personal information. Avoid sharing your real name, address, phone number, or other identifiable details.\n"
@@ -18,9 +22,38 @@ class ChannelRelay(commands.Cog):
             "- It's okay to take breaks from online interactions. Your mental health and well-being are important.\n\n"
             "Stay safe and always prioritize your safety first!"
         )
-        self.check_for_reminder.start()  # Start the periodic task
+        self.check_for_reminder.start()
 
-    @tasks.loop(minutes=1)  # Check every minute
+    def setup_database(self):
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            conn.execute('''
+            CREATE TABLE IF NOT EXISTS channels (guild_id INTEGER, channel_id INTEGER, UNIQUE(guild_id, channel_id))
+            ''')
+            conn.execute('''
+            CREATE TABLE IF NOT EXISTS last_messages (channel_id INTEGER PRIMARY KEY, last_message_time TEXT)
+            ''')
+
+    def load_channels_from_db(self):
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            return set(conn.execute('SELECT guild_id, channel_id FROM channels'))
+
+    def load_last_message_times_from_db(self):
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            return {row[0]: datetime.datetime.fromisoformat(row[1]) for row in conn.execute('SELECT channel_id, last_message_time FROM last_messages')}
+
+    def add_channel_to_db(self, guild_id, channel_id):
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            conn.execute('INSERT OR IGNORE INTO channels (guild_id, channel_id) VALUES (?, ?)', (guild_id, channel_id))
+
+    def remove_channel_from_db(self, guild_id, channel_id):
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            conn.execute('DELETE FROM channels WHERE guild_id = ? AND channel_id = ?', (guild_id, channel_id))
+
+    def update_last_message_time_in_db(self, channel_id, timestamp):
+        with sqlite3.connect(DATABASE_PATH) as conn:
+            conn.execute('INSERT OR REPLACE INTO last_messages (channel_id, last_message_time) VALUES (?, ?)', (channel_id, timestamp.isoformat()))
+
+    @tasks.loop(minutes=1)
     async def check_for_reminder(self):
         for guild_id, channel_id in self.connected_channels:
             if channel_id in self.last_message_times:
@@ -29,7 +62,7 @@ class ChannelRelay(commands.Cog):
                     channel = self.bot.get_channel(channel_id)
                     if channel:
                         await channel.send(self.safety_message)
-                        del self.last_message_times[channel_id]  # Remove the channel_id to avoid repeated reminders until a new message is sent
+                        del self.last_message_times[channel_id]
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -37,6 +70,7 @@ class ChannelRelay(commands.Cog):
             return
         if (message.guild.id, message.channel.id) in self.connected_channels:
             self.last_message_times[message.channel.id] = datetime.datetime.now()
+            self.update_last_message_time_in_db(message.channel.id, datetime.datetime.now())
             for guild_id, channel_id in self.connected_channels:
                 if guild_id != message.guild.id or channel_id != message.channel.id:
                     target_guild = self.bot.get_guild(guild_id)
@@ -48,20 +82,24 @@ class ChannelRelay(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def connect_channel(self, ctx, channel: discord.TextChannel=None):
-        """Connects a channel for relaying."""
         if not channel:
             channel = ctx.channel
         self.connected_channels.add((ctx.guild.id, channel.id))
+        self.add_channel_to_db(ctx.guild.id, channel.id)
         await ctx.send(f"Connected {channel.mention} for relaying messages.")
-    
+
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def disconnect_channel(self, ctx, channel: discord.TextChannel=None):
-        """Disconnects a channel from relaying."""
         if not channel:
             channel = ctx.channel
         self.connected_channels.discard((ctx.guild.id, channel.id))
+        self.remove_channel_from_db(ctx.guild.id, channel.id)
         await ctx.send(f"Disconnected {channel.mention} from relaying messages.")
+    
+    @commands.command()
+    async def testcommand(self, ctx):
+        await ctx.send("This is a test!")
 
 async def setup(bot):
     await bot.add_cog(ChannelRelay(bot))
