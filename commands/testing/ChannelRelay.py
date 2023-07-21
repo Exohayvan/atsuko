0552183316df1ec9cfe80340ec92df7292d045e7
Trevor Oler <18trevor3695@gmail.com>
2023-07-20 23:21:40 -0400

    Update ChannelRelay.py

--- commands/testing/ChannelRelay.py
+++ commands/testing/ChannelRelay.py
@@ -3,14 +3,14 @@ from discord.ext import commands
 class ChannelRelay(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.connected_channels = set()  # Store channel IDs
+        self.connected_channels = set()  # Store tuples of (guild_id, channel_id)
 
     # Add a channel to the connected channels list
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def connect_channel(self, ctx, channel: commands.TextChannelConverter):
         """Connects a channel for relaying."""
-        self.connected_channels.add(channel.id)
+        self.connected_channels.add((ctx.guild.id, channel.id))
         await ctx.send(f"Connected {channel.mention} for relaying messages.")
 
     # Remove a channel from the connected channels list
@@ -18,7 +18,7 @@ class ChannelRelay(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def disconnect_channel(self, ctx, channel: commands.TextChannelConverter):
         """Disconnects a channel from relaying."""
-        self.connected_channels.discard(channel.id)
+        self.connected_channels.discard((ctx.guild.id, channel.id))
         await ctx.send(f"Disconnected {channel.mention} from relaying messages.")
 
     # Relay messages from one channel to all other connected channels
@@ -26,12 +26,14 @@ class ChannelRelay(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
-        if message.channel.id in self.connected_channels:
-            for channel_id in self.connected_channels:
-                if channel_id != message.channel.id:  # Don't send it to the originating channel
-                    target_channel = self.bot.get_channel(channel_id)
-                    if target_channel:  # Ensure the channel still exists
-                        await target_channel.send(f"{message.author.display_name}: {message.content}")
+        if (message.guild.id, message.channel.id) in self.connected_channels:
+            for guild_id, channel_id in self.connected_channels:
+                if guild_id != message.guild.id or channel_id != message.channel.id:  # Don't send it to the originating channel
+                    target_guild = self.bot.get_guild(guild_id)
+                    if target_guild:
+                        target_channel = target_guild.get_channel(channel_id)
+                        if target_channel:  # Ensure the channel still exists
+                            await target_channel.send(f"{message.author.display_name}: {message.content}")
 
 async def setup(bot):
     await bot.add_cog(ChannelRelay(bot))
