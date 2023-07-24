50755fe880f096a5450097967839d3ec8544155f
Trevor Oler <18trevor3695@gmail.com>
2023-07-24 17:26:28 -0400

    Update DocGenerator.py

--- commands/docs/DocGenerator.py
+++ commands/docs/DocGenerator.py
@@ -13,15 +13,19 @@ class DocGenerator(commands.Cog):
 
     def generate_readme(self):
         """Generates or updates the readme.md file in the current directory."""
-        with open("./commands/README.md", "w") as f:
-            for cog in self.bot.cogs.values():
+        lines = []
+        for cog in self.bot.cogs.values():
+            if hasattr(cog, "get_commands"):
                 for cmd in cog.get_commands():
-                    f.write(f"## {cmd.name}\n\n")
-                    f.write(f"{cmd.help}\n\n")
+                    lines.append(f"## {cmd.name}\n\n")
+                    lines.append(f"{cmd.help}\n\n")
                     if cmd.usage:
-                        f.write(f"**Usage:**\n\n`{cmd.usage}`\n\n")
-        
+                        lines.append(f"**Usage:**\n\n`{cmd.usage}`\n\n")
+    
+        with open("./commands/README.md", "w") as f:
+            f.write(''.join(lines))
+    
         print("README.md has been generated/updated.")
-
+        
 async def setup(bot):
     await bot.add_cog(DocGenerator(bot))
