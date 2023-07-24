9675ce65b000ff6f6f1acc8eae16a37556528679
Trevor Oler <18trevor3695@gmail.com>
2023-07-24 17:11:49 -0400

    Create Cleanup.py

---
+++ commands/owner/Cleanup.py
@@ -0,0 +1,25 @@
+import os
+from discord.ext import commands
+
+class Cleanup(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def cleanup(self, ctx):
+        # Check if the user's ID matches the allowed ID
+        if ctx.author.id != 276782057412362241:
+            return
+        
+        await ctx.send("Cleaning up...")
+
+        deleted_files = 0
+        for file_name in os.listdir('.'):
+            if file_name.endswith('.png'):
+                os.remove(file_name)
+                deleted_files += 1
+        
+        await ctx.send(f"Cleaned up {deleted_files} .png files.")
+
+async def setup(bot):
+    await bot.add_cog(Cleanup(bot))
