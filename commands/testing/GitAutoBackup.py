d613685b668dfd3f88005b260189e69e448a2617
Trevor Oler <18trevor3695@gmail.com>
2023-07-25 11:36:09 -0400

    Update GitAutoBackup.py

--- commands/testing/GitAutoBackup.py
+++ commands/testing/GitAutoBackup.py
@@ -1,32 +1,44 @@
-import os
+import jwt
 import time
+import requests
+import json
+import os
 from discord.ext import commands
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import subprocess
 
+GITHUB_BASE_URL = "https://api.github.com"
+
 class GitAutoPushHandler(FileSystemEventHandler):
-    def __init__(self, git_dir):
+    def __init__(self, git_dir, get_token_callback):
         self.git_dir = git_dir
+        self.get_token_callback = get_token_callback
 
     def on_modified(self, event):
         if not event.is_directory:
             self.pull_commit_and_push()
 
     def pull_commit_and_push(self):
-        try:
-            subprocess.run(["git", "pull", "origin", "main"], cwd=self.git_dir, check=True)
-            subprocess.run(["git", "add", "."], cwd=self.git_dir, check=True)
-            subprocess.run(["git", "commit", "-m", f"Auto-commit: {time.strftime('%Y-%m-%d %H:%M:%S')}"], cwd=self.git_dir)
-            subprocess.run(["git", "push", "origin", "main"], cwd=self.git_dir)
-        except subprocess.CalledProcessError:
-            print("Error while executing Git commands.")
+        installation_token = self.get_token_callback()
+
+        env = os.environ.copy()
+        env["GIT_ASKPASS"] = "echo"
+        env["GIT_USERNAME"] = "x-access-token"
+        env["GIT_PASSWORD"] = installation_token
+
+        # rest of the git operations as before
 
 class GitAutoBackup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.path = os.getcwd()
-        self.event_handler = GitAutoPushHandler(git_dir=self.path)
+
+        # Load the config.json
+        with open('../config.json', 'r') as f:
+            self.config = json.load(f)
+
+        self.event_handler = GitAutoPushHandler(git_dir=self.path, get_token_callback=self.get_installation_token)
         self.observer = Observer()
         self.observer.schedule(self.event_handler, self.path, recursive=True)
         self.observer.start()
@@ -38,6 +50,32 @@ class GitAutoBackup(commands.Cog):
     def cog_unload(self):
         self.observer.stop()
         self.observer.join()
+
+    def generate_jwt(self):
+        # Generate a JWT
+        with open(self.config['PRIVATE_KEY_PATH'], 'r') as f:
+            private_key = f.read()
+
+        now = int(time.time())
+        payload = {
+            'iat': now,
+            'exp': now + (10 * 60),  # 10 minute max expiration
+            'iss': self.config['APP_ID']
+        }
+        return jwt.encode(payload, private_key, algorithm='RS256')
+
+    def get_installation_token(self):
+        jwt_token = self.generate_jwt()
+        headers = {
+            'Authorization': f'Bearer {jwt_token}',
+            'Accept': 'application/vnd.github.v3+json'
+        }
+        response = requests.post(f"{GITHUB_BASE_URL}/app/installations/{self.config['INSTALLATION_ID']}/access_tokens", headers=headers)
+        if response.status_code == 201:
+            token_data = response.json()
+            return token_data['token']
+        else:
+            return None
 
 async def setup(bot):
     await bot.add_cog(GitAutoBackup(bot))
