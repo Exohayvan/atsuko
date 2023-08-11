c05a8593bb6ad3ca13ac592bc5e426b1aaf447ec
Trevor Oler <18trevor3695@gmail.com>
2023-08-11 13:46:40 -0400

    Create AddBotToPlayground.py

---
+++ commands/testing/AddBotToPlayground.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+
+class AddBotToPlayground(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.channel_id = 1234567890  # Replace with the actual channel ID
+        self.role_id = 1234567890  # Replace with the actual role ID
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.bot:
+            role = member.guild.get_role(self.role_id)
+            if role:
+                await member.add_roles(role)
+                channel = self.bot.get_channel(self.channel_id)
+                if channel:
+                    await channel.send(f"Assigned role to bot: {role.name}")
+                else:
+                    print("Channel not found. Make sure the channel ID is correct in the code.")
+            else:
+                print("Role not found. Make sure the role ID is correct in the code.")
+
+    @commands.command(usage="!addbot <client ID>")
+    async def addbot(self, ctx, client_id: int):
+        """Adds a bot using its client ID."""
+        invite_link = f"https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions=0&scope=bot"
+        channel = self.bot.get_channel(self.channel_id)
+        if channel:
+            await channel.send(f"Invite this bot using the following link: {invite_link}")
+        else:
+            await ctx.send("Channel not found. Make sure the channel ID is correct in the code.")
+
+async def setup(bot):
+    bot.add_cog(AddBotToPlayground(bot))
