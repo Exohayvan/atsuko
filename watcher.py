c5e6f77eb71da9151659d01f0502a4c4b8a15d8b
Trevor Oler <18trevor3695@gmail.com>
2023-09-27 18:31:11 -0400

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -1,10 +1,6 @@
 # watcher.py
 import subprocess
 
-RESTART_EXIT_CODE = 42  # or any number you choose
-
 while True:
     process = subprocess.Popen(["python3", "main.py"])
-    exit_code = process.wait()
-    if exit_code != RESTART_EXIT_CODE:
-        break
+    process.wait()
