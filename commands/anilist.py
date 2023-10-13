5cc9f851ec3ecfebd982f0d64ca7d4f2553c2350
Trevor Oler <18trevor3695@gmail.com>
2023-10-12 23:29:56 -0400

    Update anilist.py

--- commands/anilist.py
+++ commands/anilist.py
@@ -90,39 +90,25 @@ class AniList(commands.Cog):
         """Fetches the user's or mentioned user's stats from AniList."""
         if user is None:
             user = ctx.author
-
+    
         self.c.execute("SELECT username FROM usernames WHERE id=?", (user.id,))
         result = self.c.fetchone()
-
+    
         if result is not None:
             username = result[0]
         else:
             await ctx.send(f"{user.mention} has not set their AniList username.")
             return
-
+    
         query = '''
         query ($username: String) {
             User(name: $username) {
                 stats {
                     anime {
-                        episodesWatched
-                        minutesWatched
                         statuses {
                             watching
                             completed
                             planning
-                        }
-                        genres(limit: 3)
-                        tags(limit: 3)
-                        staff(limit: 3) {
-                            id
-                            name {
-                                full
-                            }
-                        }
-                        studios(limit: 3) {
-                            id
-                            name
                         }
                     }
                 }
@@ -130,34 +116,23 @@ class AniList(commands.Cog):
         }
         '''
         variables = {'username': username}
-
+    
         response = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': variables})
-
+    
         if response.status_code == 200:
             data = response.json()
             anime_stats = data['data']['User']['stats']['anime']
-
+    
             embed = discord.Embed(title=f"{user}'s AniList Stats", color=discord.Color.blue())
-
+    
             # Anime Statuses
             embed.add_field(name="Animes Watching", value=anime_stats['statuses']['watching'], inline=True)
             embed.add_field(name="Animes Watched", value=anime_stats['statuses']['completed'], inline=True)
             embed.add_field(name="Animes Planned", value=anime_stats['statuses']['planning'], inline=True)
-
-            # Episodes and Days
-            embed.add_field(name="Episodes Watched", value=anime_stats['episodesWatched'], inline=True)
-            embed.add_field(name="Days Watched", value=anime_stats['minutesWatched'] / 1440, inline=True) # 1440 minutes = 1 day
-            # Note: Days Planned is not directly available from AniList's API without more complex querying.
-
-            # Genres, Tags, Voice Actors, Studios
-            embed.add_field(name="Top 3 Genres", value=", ".join(anime_stats['genres']), inline=True)
-            embed.add_field(name="Top 3 Tags", value=", ".join([tag['name'] for tag in anime_stats['tags']]), inline=True)
-            embed.add_field(name="Top 3 Voice Actors", value=", ".join([staff['name']['full'] for staff in anime_stats['staff']]), inline=True)
-            embed.add_field(name="Top 3 Studios", value=", ".join([studio['name'] for studio in anime_stats['studios']]), inline=True)
-
+    
             await ctx.send(embed=embed)
         else:
             await ctx.send("Failed to fetch stats.")
-            
+                    
 async def setup(bot):
     await bot.add_cog(AniList(bot))
