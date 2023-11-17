da32197ceed0152af008d9dfeaf1875eb880f6f4
Trevor Oler <18trevor3695@gmail.com>
2023-11-17 11:21:46 -0500

    Update CommandError.py

--- commands/errorhandling/CommandError.py
+++ commands/errorhandling/CommandError.py
@@ -1,14 +1,12 @@
 from discord.ext import commands
-import json
-from github import GithubIntegration
-from discord.ext.commands import CheckFailure
+from discord import Embed, Forbidden
 import github
-import traceback
-import sys, platform
-import discord
-from discord import Embed
-import os
+import json
 import logging
+import sys
+import platform
+import discord
+import traceback
 
 def get_config():
     with open('../config.json', 'r') as f:
@@ -28,8 +26,8 @@ class CommandError(commands.Cog):
         self.bot = bot
         self.private_key_path = private_key_path
         self.github_repo = "Exohayvan/atsuko"
-        self.app_id = config.get('APP_ID')  # You should store your app id in the config file
-        self.installation_id = config.get('INSTALLATION_ID')  # You should store your installation id in the config file
+        self.app_id = config.get('APP_ID')
+        self.installation_id = config.get('INSTALLATION_ID')
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
@@ -38,45 +36,41 @@ class CommandError(commands.Cog):
         if isinstance(error, CheckFailure):
             print("CheckFailure caught.")
             return
-        if isinstance(error, commands.CommandInvokeError):
-            return
-        if isinstance(error.original, Forbidden):
-            print(f"Forbidden - {ctx.guild.name}")
-            return
-        if isinstance(error, commands.CommandError):
-            tb = traceback.format_exception(type(error), error, error.__traceback__)
-            traceback_str = "".join(tb).strip()  # remove leading/trailing white spaces
 
-            issue_title = f"Auto Generated Report: {str(error)}"
-            issue_body = (f"**User Message:** {ctx.message.content}\n"
-                  f"**Error:** {str(error)}\n"
-                  f"**Traceback:** \n```python\n{traceback_str}\n```\n"  # extra newline here
-                  f"**Command:** `{ctx.command.qualified_name}`\n"  # use inline code format for single line code
-                  f"**Author:** {ctx.author}\n"
-                  f"**Channel:** {ctx.channel}\n"
-                  f"**Python Version:** `{sys.version}`\n"  # use inline code format for single line code
-                  f"**discord.py Version:** `{discord.__version__}`\n"  # use inline code format for single line code
-                  f"**OS:** `{platform.system()} {platform.release()}`")  # use inline code format for single line code
+        error_message = str(error.original) if hasattr(error, 'original') else str(error)
+        tb = traceback.format_exception(type(error), error, error.__traceback__)
+        traceback_str = "".join(tb).strip()
 
-            # Use GithubIntegration for app authentication
-            private_key = open(self.private_key_path, 'r').read()
-            integration = GithubIntegration(self.app_id, private_key)
-            token = integration.get_access_token(self.installation_id)
+        issue_title = f"Auto Generated Report: {error_message}"
+        issue_body = (f"**User Message:** {ctx.message.content}\n"
+                      f"**Error:** {error_message}\n"
+                      f"**Traceback:** \n```python\n{traceback_str}\n```\n"
+                      f"**Command:** `{ctx.command.qualified_name}`\n"
+                      f"**Author:** {ctx.author}\n"
+                      f"**Channel:** {ctx.channel}\n"
+                      f"**Python Version:** `{sys.version}`\n"
+                      f"**discord.py Version:** `{discord.__version__}`\n"
+                      f"**OS:** `{platform.system()} {platform.release()}`")
 
-            try:
-                g = github.Github(token.token)
-                repo = g.get_repo(self.github_repo)
-                issue = repo.create_issue(title=issue_title, body=issue_body)
-                bug_label = repo.get_label("bug")  # get the label from the repo
-                issue.add_to_labels(bug_label)  # add the label to the issue
-            
-                embed = Embed(title='An error occurred', color=0xff0000)
-                embed.add_field(name='Issue created on GitHub', value=f'[Link to issue]({issue.html_url})', inline=False)
-                await ctx.send(embed=embed)
-            except Exception as e:
-                await ctx.sent(f"I am unable to open an issue on GitHub.")
-                await ctx.send(f"An unexpected error occurred: {e}")
-                        
+        # Use GithubIntegration for app authentication
+        private_key = open(self.private_key_path, 'r').read()
+        integration = github.GithubIntegration(self.app_id, private_key)
+        token = integration.get_access_token(self.installation_id)
+
+        try:
+            g = github.Github(token.token)
+            repo = g.get_repo(self.github_repo)
+            issue = repo.create_issue(title=issue_title, body=issue_body)
+            bug_label = repo.get_label("bug")
+            issue.add_to_labels(bug_label)
+
+            embed = Embed(title='An error occurred', color=0xff0000)
+            embed.add_field(name='Issue created on GitHub', value=f'[Link to issue]({issue.html_url})', inline=False)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"I am unable to open an issue on GitHub.")
+            await ctx.send(f"An unexpected error occurred: {e}")
+
 async def setup(bot):
     config = get_config()
     private_key_path = config.get('PRIVATE_KEY_PATH')
