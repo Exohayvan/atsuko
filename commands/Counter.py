bf2432866c5f2ad2f630a215ef5df8ad623787d4
Trevor Oler <18trevor3695@gmail.com>
2023-12-14 16:56:58 -0500

    Update Counter.py

--- commands/Counter.py
+++ commands/Counter.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands, tasks
 import sqlite3
-import os
 
 class Counter(commands.Cog):
     def __init__(self, bot):
@@ -13,6 +12,7 @@ class Counter(commands.Cog):
 
     @tasks.loop(minutes=10)
     async def update_counters(self):
+        print("Updating counters")  # Debug log
         guilds = self.bot.guilds
         conn = sqlite3.connect('./data/db/counter.db')
         c = conn.cursor()
@@ -33,27 +33,33 @@ class Counter(commands.Cog):
     
         conn.commit()
         conn.close()
-    
+
     async def update_counter(self, channel):
-        guild = channel.guild
-        name = channel.name
-    
-        if "Total members" in name:
-            count = len(guild.members)
-            new_name = f"Total members: {count}"
-        elif "Online members" in name:
-            count = len([member for member in guild.members if member.status != discord.Status.offline])
-            new_name = f"Online members: {count}"
-        elif "Bots" in name:
-            count = len([member for member in guild.members if member.bot])
-            new_name = f"Bots: {count}"
-        else:
-            return  # If none of the keywords match, do nothing
-    
-        await channel.edit(name=new_name)
+        try:
+            guild = channel.guild
+            name = channel.name
         
-    @commands.command(usage="!create_counter \"Counter Type\" (Must be inclosed in \"\". Also caps are needed. This is a bug will fix later.)")
-    async def create_counter(self, ctx, name):
+            if "Total members" in name:
+                count = len(guild.members)
+                new_name = f"Total members: {count}"
+            elif "Online members" in name:
+                count = len([member for member in guild.members if member.status != discord.Status.offline])
+                new_name = f"Online members: {count}"
+            elif "Bots" in name:
+                count = len([member for member in guild.members if member.bot])
+                new_name = f"Bots: {count}"
+            else:
+                return  # If none of the keywords match, do nothing
+        
+            if channel.name != new_name:  # Only update if the name has changed
+                await channel.edit(name=new_name)
+                print(f"Updated channel: {channel.name}")  # Debug log
+
+        except Exception as e:
+            print(f"Error updating channel: {e}")  # Exception handling
+
+    @commands.command()
+    async def create_counter(self, ctx, *, name):
         """Creates a new counter voice channel with the given name. Counter types: Total members, Online members, Bots."""
         if name not in ["Total members", "Online members", "Bots"]:
             await ctx.send("Invalid counter name. Must be one of 'Total members', 'Online members', 'Bots'.")
@@ -62,7 +68,7 @@ class Counter(commands.Cog):
         channel = await ctx.guild.create_voice_channel(name + ": 0", category=ctx.channel.category)
         
         # Store the channel in the database
-        conn = sqlite3.connect('./data/counter.db')
+        conn = sqlite3.connect('./data/db/counter.db')
         c = conn.cursor()
         c.execute('CREATE TABLE IF NOT EXISTS Counter (guild_id int, channel_id int)')
         c.execute('INSERT INTO Counter VALUES (?, ?)', (ctx.guild.id, channel.id))
@@ -70,6 +76,11 @@ class Counter(commands.Cog):
         conn.close()
 
         await self.update_counter(channel)
+
+    # Manual command to trigger counter update
+    @commands.command(name='forceupdate')
+    async def force_update(self, ctx):
+        await self.update_counters()
 
 async def setup(bot):
     await bot.add_cog(Counter(bot))
