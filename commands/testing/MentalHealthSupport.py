c393657de87a1df8fbd6e194a86b098300e0de07
Trevor Oler <18trevor3695@gmail.com>
2023-12-21 05:38:26 -0500

    Update MentalHealthSupport.py

--- commands/testing/MentalHealthSupport.py
+++ commands/testing/MentalHealthSupport.py
@@ -1,28 +1,20 @@
 from discord.ext import commands
 import discord
-import os
 
 class MentalHealthSupportCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.trigger_words = self.load_trigger_words()
-
-    def load_trigger_words(self):
-        trigger_words = []
-        directory = '.data/txt/mentaltriggers'
-        
-        # Loop through each file in the directory
-        for filename in os.listdir(directory):
-            if filename.endswith('.txt'):
-                with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
-                    # Add each line as a trigger word
-                    trigger_words.extend(line.strip() for line in file if line.strip())
-        return trigger_words
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        # Specific list of trigger words
+        trigger_words = [
+            "kms", "suicide", "self harm", "selfharm", "end my life", 
+            "kill myself", "harming myself", "no one cares", "give up"
+        ]
+
         # Check if any trigger word is in the message and if the author is not the bot
-        if any(word in message.content.lower() for word in self.trigger_words) and message.author != self.bot.user:
+        if any(word in message.content.lower() for word in trigger_words) and message.author != self.bot.user:
             embed = discord.Embed(
                 title="It's Okay to Ask for Help",
                 description=("It seems like you might be going through a tough time. If you need someone to talk to, there are people who can help. "
