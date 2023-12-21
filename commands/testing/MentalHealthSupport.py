6531b5322fdba8d8b779e73149974e204621b925
Trevor Oler <18trevor3695@gmail.com>
2023-12-21 05:15:49 -0500

    Update MentalHealthSupport.py

--- commands/testing/MentalHealthSupport.py
+++ commands/testing/MentalHealthSupport.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 class MentalHealthSupportCog(commands.Cog):
     def __init__(self, bot):
@@ -14,10 +15,16 @@ class MentalHealthSupportCog(commands.Cog):
 
         # Check if any trigger word is in the message and if the author is not the bot
         if any(word in message.content.lower() for word in trigger_words) and message.author != self.bot.user:
-            response = ("It seems like you might be going through a tough time. If you need someone to talk to, there are people who can help. "
-                        "Here are some resources and support numbers: "
-                        "https://github.com/Exohayvan/atsuko/blob/main/documents/Crisis-Support-Numbers.md")
-            await message.channel.send(response)
+            embed = discord.Embed(
+                title="It's Okay to Ask for Help",
+                description=("It seems like you might be going through a tough time. If you need someone to talk to, there are people who can help. "
+                             "Here are some resources and support numbers."),
+                color=discord.Color.blue()
+            )
+            embed.add_field(name="Crisis Support Numbers", value="[Click here for resources](https://github.com/Exohayvan/atsuko/blob/main/documents/Crisis-Support-Numbers.md)", inline=False)
+            embed.set_footer(text="Remember, you are not alone.")
+
+            await message.channel.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(MentalHealthSupportCog(bot))
