41198da6a032652982f5b521d60c60a52c485b2a
Trevor Oler <18trevor3695@gmail.com>
2024-01-15 12:24:23 -0500

    Update EmojiManager.py

--- commands/testing/EmojiManager.py
+++ commands/testing/EmojiManager.py
@@ -45,19 +45,36 @@ class EmojiManager(commands.Cog):
             else:
                 await ctx.send(f"Please provide a valid custom emoji for {emoji_str}.")
                             
-    @commands.command(usage="!emoji_add <emoji>")
+    @commands.command(usage="!emoji_add <emoji1> <emoji2> ...")
     @commands.has_permissions(manage_guild=True)
-    async def emoji_add(self, ctx, emoji: discord.PartialEmoji):
-        """Saves an emoji to local storage and adds it to the server."""
-        await self.emoji_save(ctx, emoji)
+    async def emoji_add(self, ctx, *emojis):
+        """Saves multiple custom emojis to local storage and adds them to the server."""
+        for emoji_str in emojis:
+            if emoji_str.startswith("<") and emoji_str.endswith(">"):
+                animated, emoji_name, emoji_id = False, '', ''
+                split = emoji_str.strip('<>').split(':')
+                if len(split) == 3:
+                    animated, emoji_name, emoji_id = split[0] == 'a', split[1], split[2]
 
-        with open(os.path.join(self.emoji_folder, f"{emoji.name}.{'gif' if emoji.animated else 'png'}"), 'rb') as emoji_file:
-            try:
-                await ctx.guild.create_custom_emoji(name=emoji.name, image=emoji_file.read())
-                await ctx.send(f"Emoji {emoji.name} added to the server!")
-            except discord.HTTPException as e:
-                await ctx.send(f"Failed to add emoji to the server: {e}")
+                if emoji_id.isdigit():
+                    file_extension = 'gif' if animated else 'png'
+                    file_path = os.path.join(self.emoji_folder, f"{emoji_name}.{file_extension}")
+                    emoji_url = f"https://cdn.discordapp.com/emojis/{emoji_id}.{file_extension}"
 
+                    if not os.path.exists(file_path):
+                        await self.download_emoji(emoji_url, file_path)
+
+                    with open(file_path, 'rb') as emoji_file:
+                        try:
+                            await ctx.guild.create_custom_emoji(name=emoji_name, image=emoji_file.read())
+                            await ctx.send(f"Emoji {emoji_name} added to the server!")
+                        except discord.HTTPException as e:
+                            await ctx.send(f"Failed to add emoji {emoji_name} to the server: {e}")
+                else:
+                    await ctx.send(f"Invalid emoji format for {emoji_str}.")
+            else:
+                await ctx.send(f"Please provide a valid custom emoji for {emoji_str}.")
+                
     # Error handling for permissions
     async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
