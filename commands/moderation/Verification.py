2c2042b58decd57ff76e7081badee851e0f4f245
Trevor Oler <18trevor3695@gmail.com>
2024-02-13 23:52:50 -0500

    Update Verification.py

--- commands/moderation/Verification.py
+++ commands/moderation/Verification.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import sqlite3
 import random
 from discord.ext.commands import MissingRequiredArgument
+import os
 
 class Verification(commands.Cog):
     def __init__(self, bot):
