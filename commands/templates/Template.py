7af8ad46f0b3a04de4c5df68b0af9d3e329b4a32
ExoHayvan <18trevor3695@gmail.com>
2024-02-17 20:33:45 -0500

    hidding template from help command

--- commands/templates/Template.py
+++ commands/templates/Template.py
@@ -5,7 +5,7 @@ class Template(commands.Cog):
         self.bot = bot
 
     # Placeholder command
-    @commands.command(usage="!placeholder <usage>")
+    @commands.command(usage="!placeholder <usage>", hidden=True) # Remove "hidden=True" for new commands 
     async def placeholder(self, ctx):
         """This is a placeholder command."""
         await ctx.send("This is a placeholder command. Replace it with your own implementation!")
