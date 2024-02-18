6362ca470504add95a9bf522f806e42ca5e2e545
ExoHayvan <18trevor3695@gmail.com>
2024-02-17 23:01:55 -0500

    Update PullLog.py

--- commands/owner/PullLog.py
+++ commands/owner/PullLog.py
@@ -23,7 +23,7 @@ class PullLog(commands.Cog):
         if log_filename:
             # Send specific log file
             file_path = os.path.join(self.log_directory, log_filename)
-            file_path = 'f{filepath}.log'
+            file_path = str('f{filepath}.log')
             if os.path.exists({file_path}):
                 await ctx.send(file=discord.File(file_path))
                 logger.info(f"Sent log file: {log_filename}")
