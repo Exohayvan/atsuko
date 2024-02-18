726ad343aae4df89dc23b20fb4250ed1175ff23e
ExoHayvan <18trevor3695@gmail.com>
2024-02-17 22:48:49 -0500

    added logpuller

--- commands/templates/Template.py
+++ commands/templates/Template.py
@@ -1,4 +1,13 @@
 from discord.ext import commands
+import logging
+
+logger = logging.getLogger('Template.py')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='./logs/Template.py.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+logger.propagate = False
+logger.info("Template Cog Loaded. Logging started...")
 
 class Template(commands.Cog):
     def __init__(self, bot):
@@ -9,6 +18,7 @@ class Template(commands.Cog):
     async def placeholder(self, ctx):
         """This is a placeholder command."""
         await ctx.send("This is a placeholder command. Replace it with your own implementation!")
+        logger.info("Template command ran.")
 
 async def setup(bot):
     await bot.add_cog(Template(bot))
