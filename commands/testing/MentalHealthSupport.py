06da3ead973fb140fd316237537dcfe38cc4eba7
Trevor Oler <18trevor3695@gmail.com>
2024-02-25 00:12:22 -0500

    Update MentalHealthSupport.py

--- commands/testing/MentalHealthSupport.py
+++ commands/testing/MentalHealthSupport.py
@@ -4,18 +4,31 @@ import discord
 class MentalHealthSupportCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.trigger_words = self.load_trigger_words()
+
+    def load_trigger_words(self):
+        # Initialize an empty list to store trigger words
+        trigger_words = []
+        # Define the path to the file
+        file_path = './data/txt/mentaltriggers/english.txt'
+        # Open the file and read each line
+        try:
+            with open(file_path, 'r') as file:
+                for line in file:
+                    # Strip whitespace and add the trigger word to the list
+                    trigger_word = line.strip()
+                    if trigger_word:  # Ensure the line is not empty
+                        trigger_words.append(trigger_word.lower())  # Add trigger word in lowercase to ensure case-insensitive matching
+        except FileNotFoundError:
+            print(f"Error: The file at {file_path} was not found.")
+        except Exception as e:
+            print(f"An error occurred while reading {file_path}: {e}")
+        return trigger_words
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Specific list of trigger words
-        trigger_words = [
-            "kms", "suicide", "self harm", "selfharm", "end my life", 
-            "kill myself", "harming myself", "no one cares", "give up",
-            "killing myself"
-        ]
-
         # Check if any trigger word is in the message and if the author is not the bot
-        if any(word in message.content.lower() for word in trigger_words) and message.author != self.bot.user:
+        if any(word in message.content.lower() for word in self.trigger_words) and message.author != self.bot.user:
             embed = discord.Embed(
                 title="It's Okay to Ask for Help",
                 description=("It seems like you might be going through a tough time. If you need someone to talk to, there are people who can help. "
