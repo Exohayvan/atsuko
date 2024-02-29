2bb3a37fade0ceafa1b34961f3fca963cecca6a0
Trevor Oler <18trevor3695@gmail.com>
2024-02-28 21:46:03 -0500

    Update Template.py

--- commands/templates/Template.py
+++ commands/templates/Template.py
@@ -1,5 +1,6 @@
-from discord.ext import commands
 import discord
+from discord import app_commands
+from discord.ext import commands
 import logging
 
 # Initialize logging
@@ -21,5 +22,24 @@ class Template(commands.Cog):
         await interaction.response.send_message("This is a placeholder slash command. Replace it with your own implementation!")
         logger.info("Template slash command ran.")
 
-async def setup(bot: commands.Bot):
-    await bot.add_cog(Template(bot))
+class MyCog(commands.Cog):
+  def __init__(self, bot: commands.Bot) -> None:
+    self.bot = bot
+    
+  group = app_commands.Group(name="parent", description="...")
+  # Above, we declare a command Group, in discord terms this is a parent command
+  # We define it within the class scope (not an instance scope) so we can use it as a decorator.
+  # This does have namespace caveats but i don't believe they're worth outlining in our needs.
+
+  @group.command(name="top-command")
+  async def my_top_command(self, interaction: discord.Interaction) -> None:
+    """ /top-command """
+    await interaction.response.send_message("Hello from top level command!", ephemeral=True)
+
+  @group.command(name="sub-command") # we use the declared group to make a command.
+  async def my_sub_command(self, interaction: discord.Interaction) -> None:
+    """ /parent sub-command """
+    await interaction.response.send_message("Hello from the sub command!", ephemeral=True)
+
+async def setup(bot: commands.Bot) -> None:
+  await bot.add_cog(MyCog(bot))
