67ecf134e69afb67cb38ff3fe1545ddd932dfd9b
Trevor Oler <18trevor3695@gmail.com>
2024-02-28 22:11:57 -0500

    Update Template.py

--- commands/templates/Template.py
+++ commands/templates/Template.py
@@ -15,24 +15,24 @@ logger.info("Template Cog Loaded. Logging started...")
 class Template(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-      
+    
+    group = app_commands.Group(name="TemplateGroup", description="...")
+    # Above, we declare a command Group, in discord terms this is a parent command
+    # We define it within the class scope (not an instance scope) so we can use it as a decorator.
+    # This does have namespace caveats but i don't believe they're worth outlining in our needs.
+
     @app_commands.command(name="placeholder", description="This is a placeholder command.")
     async def placeholder(self, interaction: discord.Interaction):
         """This is a placeholder slash command. Replace it with your own implementation!"""
         await interaction.response.send_message("This is a placeholder slash command. Replace it with your own implementation!")
         logger.info("Template slash command ran.")
-    
-    group = app_commands.Group(name="parent", description="...")
-    # Above, we declare a command Group, in discord terms this is a parent command
-    # We define it within the class scope (not an instance scope) so we can use it as a decorator.
-    # This does have namespace caveats but i don't believe they're worth outlining in our needs.
-
-    @group.command(name="top-command")
+        
+    @group.command(name="top")
     async def my_top_command(self, interaction: discord.Interaction) -> None:
         """ /top-command """
         await interaction.response.send_message("Hello from top level command!", ephemeral=True)
 
-    @group.command(name="sub-command") # we use the declared group to make a command.
+    @group.command(name="bottom") # we use the declared group to make a command.
     async def my_sub_command(self, interaction: discord.Interaction) -> None:
         """ /parent sub-command """
         await interaction.response.send_message("Hello from the sub command!", ephemeral=True)
