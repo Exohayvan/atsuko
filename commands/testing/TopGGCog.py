0a832c18e5f356198967cfa0440c91f5c4f06f13
Trevor Oler <18trevor3695@gmail.com>
2024-02-29 23:14:36 -0500

    Update TopGGCog.py

--- commands/testing/TopGGCog.py
+++ commands/testing/TopGGCog.py
@@ -32,7 +32,7 @@ class TopGGCog(commands.Cog):
     @tasks.loop(hours=6)  # Run every 6 hours
     async def server_count_post(self):
         await self.bot.wait_until_ready()
-        await self.post_server_count()
+        self.post_server_count()
 
 def setup(bot):
     bot.add_cog(TopGGCog(bot))
