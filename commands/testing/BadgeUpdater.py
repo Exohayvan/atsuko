4610b59f382e4b27b13b4824458a87f5a8c48808
Trevor Oler <18trevor3695@gmail.com>
2024-03-02 22:56:07 -0500

    Update BadgeUpdater.py

--- commands/testing/BadgeUpdater.py
+++ commands/testing/BadgeUpdater.py
@@ -39,5 +39,5 @@ class BadgeUpdater(commands.Cog):
     async def before_update_badges(self):
         await self.bot.wait_until_ready()
 
-def setup(bot):
-    bot.add_cog(BadgeUpdater(bot))
+async def setup(bot):
+    await bot.add_cog(BadgeUpdater(bot))
