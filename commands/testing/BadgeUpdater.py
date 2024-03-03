d0819205bab89b0b8cef38e2fbd6689cfd198edd
Trevor Oler <18trevor3695@gmail.com>
2024-03-02 21:39:13 -0500

    Create BadgeUpdater.py

---
+++ commands/testing/BadgeUpdater.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands, tasks
+import os
+import asyncio
+
+class BadgeUpdater(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.update_badges.start()
+
+    def cog_unload(self):
+        self.update_badges.cancel()
+
+    async def update_servers_txt(self):
+        server_count = len(self.bot.guilds)
+        with open('.github/badges/servers.txt', 'w') as file:
+            file.write(str(server_count))
+
+    async def update_users_txt(self):
+        user_count = len(set(self.bot.get_all_members()))
+        with open('.github/badges/users.txt', 'w') as file:
+            file.write(str(user_count))
+
+    @tasks.loop(minutes=30)
+    async def update_badges(self):
+        await self.update_servers_txt()
+        await self.update_users_txt()
+
+    @update_badges.before_loop
+    async def before_update_badges(self):
+        await self.bot.wait_until_ready()
+
+async def setup(bot):
+    cog = BadgeUpdater(bot)
+    bot.add_cog(cog)
+    await cog.before_update_badges()
