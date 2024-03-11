c5df9675abb941f41b1bd6f9e4c39282be2aef21
ExoHayvan <18trevor3695@gmail.com>
2024-03-11 19:07:14 -0400

    updated some runtime info :>

--- runtime/main.py
+++ runtime/main.py
@@ -2,17 +2,14 @@ import json
 import subprocess
 from time import sleep
 
-# Paths to your scripts
 scripts = {
-    "script1": "path/to/script1.py",
-    "script2": "path/to/script2.py"
+    "bot": "../bot/app.py",
+    "web": "../web/app.py"
 }
-
-# Process handles
 processes = {}
 
 def read_config():
-    with open('config.json', 'r') as file:
+    with open('run.json', 'r') as file:
         return json.load(file)
 
 def manage_scripts():
@@ -21,16 +18,14 @@ def manage_scripts():
     for script, should_run in config.items():
         if should_run:
             if script not in processes or processes[script].poll() is not None:
-                # Start or restart the script
                 print(f"Starting {script}...")
                 processes[script] = subprocess.Popen(['python', scripts[script]])
         else:
             if script in processes:
-                # Stop the script
                 print(f"Stopping {script}...")
                 processes[script].kill()
 
 if __name__ == "__main__":
     while True:
         manage_scripts()
-        sleep(10)  # Check every 10 seconds
+        sleep(10)
